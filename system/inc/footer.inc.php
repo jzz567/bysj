d595bb67bb1bbb6b8b3372071238f05229c0cd17
jzz <jzz15@vip.qq.com>
2016-03-12 15:13:44 +0800

    添加初始化说明

--- system/inc/footer.inc.php
+++ system/inc/footer.inc.php
@@ -12,7 +12,7 @@
 <script src="http://jzz15.oss-cn-shenzhen.aliyuncs.com/js/pusher.min.js" type="text/javascript"></script>
 <script src="http://jzz15.oss-cn-shenzhen.aliyuncs.com/js/jquery.min.js" type="text/javascript"></script>
 <script type="text/javascript">
-    var pusher = new Pusher('cea0285687ebb980bc16');
+    var pusher = new Pusher('cea0285687ebb980bc16');//这里要替换成你自己的PUSHER_KEY
     var channel = pusher.subscribe('<?php echo $channel;?>');
 </script>
 <script type="text/javascript" src="<?php echo APP_URI; ?>assets/scripts/init.js"></script>
